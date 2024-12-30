c0297692e8ee4e97ba08d4581fa7311e6e095786
roofers_00_dribble <roofers_00_dribble@icloud.com>
2024-12-30 14:39:04 +0800

    feat: sending transcation

--- environment.d.ts
+++ environment.d.ts
@@ -4,10 +4,11 @@ declare global {
       REDIS_URL: string
       SOLANA_RPC_URL: string
       TELEGRAM_BOT_SECRET: string
+      WALLET_PRIVATE_KEY: string
     }
   }
 }
 
 // If this file has no import/export statements (i.e. is a script)
 // convert it into a module by adding an empty export statement.
-export { }
+export {}
--- src/services/trading.ts
+++ src/services/trading.ts
@@ -1,4 +1,10 @@
-import { Connection, PublicKey } from '@solana/web3.js'
+import { Connection, Keypair, PublicKey, VersionedTransaction } from '@solana/web3.js'
+import fetch from 'cross-fetch'
+import { Wallet } from '@project-serum/anchor'
+import bs58 from 'bs58'
+
+const SOL_MINT_ADDRESS = 'So11111111111111111111111111111111111111112'
+const SOL_AMOUNT = 500000
 
 export default class Trading {
   private readonly _connection: Connection
@@ -12,6 +18,10 @@ export default class Trading {
     const supply = await this._connection.getTokenSupply(mintAddress)
     const topHolders = await this._connection.getTokenLargestAccounts(mintAddress)
 
+    if (!address) {
+      return
+    }
+
     const tokenInfo = {
       address,
       amount: supply.value.uiAmount,
@@ -24,6 +34,61 @@ export default class Trading {
       })
     }
 
-    console.log(tokenInfo)
+    const wallet = new Wallet(Keypair.fromSecretKey(bs58.decode(process.env.WALLET_PRIVATE_KEY || '')))
+
+    // Swapping SOL to Mint Address with input 0.001 SOL and 10% slippage
+    const quoteResponse = await (
+      await fetch(`https://quote-api.jup.ag/v6/quote?inputMint=${SOL_MINT_ADDRESS}&outputMint=${address}&amount=${SOL_AMOUNT}&slippageBps=1000`
+      )
+    ).json()
+
+    // get serialized transactions for the swap
+    const { swapTransaction } = await (
+      await fetch('https://quote-api.jup.ag/v6/swap', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          // quoteResponse from /quote api
+          quoteResponse,
+          // user public key to be used for the swap
+          userPublicKey: wallet.publicKey.toString(),
+          // auto wrap and unwrap SOL. default is true
+          wrapAndUnwrapSol: true
+          // feeAccount is optional. Use if you want to charge a fee.  feeBps must have been passed in /quote API.
+          // feeAccount: "fee_account_public_key"
+        })
+      })
+    ).json()
+
+    // deserialize the transaction
+    const swapTransactionBuf = Buffer.from(swapTransaction, 'base64')
+    const transaction = VersionedTransaction.deserialize(swapTransactionBuf)
+
+    // sign the transaction
+    transaction.sign([wallet.payer])
+
+    // get the latest block hash
+    const latestBlockHash = await this._connection.getLatestBlockhash()
+
+    try {
+      // Execute the transaction
+      const rawTransaction = transaction.serialize()
+      const txid = await this._connection.sendRawTransaction(rawTransaction, {
+        skipPreflight: false
+      })
+
+      console.log('txid', txid)
+
+      // const confirmInfo = await this._connection.confirmTransaction({
+      //   blockhash: latestBlockHash.blockhash,
+      //   lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+      //   signature: txid
+      // })
+      // console.log(txid, confirmInfo)
+    } catch (e) {
+      console.log('error', e)
+    }
   }
 }
