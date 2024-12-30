c8dfbac2456902041ae270a5f03c5b5a171ade7c
roofers_00_dribble <roofers_00_dribble@icloud.com>
2024-12-31 00:47:54 +0800

    fix: adjust price

--- src/api/gmgn.ts
+++ src/api/gmgn.ts
@@ -1,5 +1,4 @@
 import { get } from 'lodash'
-import chalk from 'chalk'
 
 /**
  * 获取钱包利润
@@ -21,17 +20,11 @@ export const getWalletProfit = async (walletAddress: string) => {
     return holdings.map(v => {
       const profit = Number(v.realized_profit)
       return {
-        ...v,
-        profit
-      }
-    }).sort((a, b) => b.profit - a.profit).map(v => {
-      const profit = Number(v.profit)
-      return {
         symbol: v.token.symbol,
         tokenAddress: v.token.token_address,
-        profit: profit > 0 ? chalk.green(profit.toFixed(2)) : chalk.red(profit.toFixed(2))
+        profit
       }
-    })
+    }).sort((a, b) => b.profit - a.profit)
   }
 
   return []
