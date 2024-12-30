9fa7914f5116a27f5cadcfc5adf126c03cdf48dd
roofers_00_dribble <roofers_00_dribble@icloud.com>
2024-12-31 00:56:35 +0800

    feat: display wallet profit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import chalk from 'chalk'
+import { pick } from 'lodash'
 import * as dotenv from 'dotenv'
 import { Connection } from '@solana/web3.js'
 import Trading from './services/trading'
+import { getWalletProfit } from './api/gmgn'
 
 (async () => {
   dotenv.config({
@@ -9,4 +12,18 @@ import Trading from './services/trading'
 
   const connection = new Connection(process.env.SOLANA_RPC_URL, 'confirmed')
   const trading = new Trading({ connection })
+  const profitList = await getWalletProfit('')
+  const coloredList = profitList.map(v => {
+    const profit = Number(v.profit)
+    return {
+      ...pick(v,[
+        'symbol',
+        'tokenAddress',
+      ]),
+      profit: profit >= 0 ? chalk.green(profit.toFixed(2)) : chalk.red(Math.abs(profit).toFixed(2)),
+      holding: v.balance <= 0 ? chalk.white('SOLD') : chalk.yellowBright('HODL')
+    }
+  })
+
+  console.table(coloredList)
 })()
