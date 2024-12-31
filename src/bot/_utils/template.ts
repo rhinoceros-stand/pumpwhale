641b71039a45eb902823c69a53a93e991345395e
roofers_00_dribble <roofers_00_dribble@icloud.com>
2024-12-31 17:20:43 +0800

    feat: add message template

---
+++ src/bot/_utils/template.ts
@@ -0,0 +1,38 @@
+import { bold, format, link } from '@gramio/format'
+
+/**
+ * 输出 Bonding 消息
+ * @param name
+ * @param symbol
+ * @param mintAddress
+ */
+export function renderBonding(name: string, symbol: string, mintAddress: string) {
+  return format`
+   ${bold(name)}($${bold(symbol)})
+   ${bold(mintAddress)}
+   Links: ${link(
+    'Pump.fun', `https://pump.fun/coin/${mintAddress}`
+  )} | ${link(
+    'GMGN.ai', `https://gmgn.ai/sol/token/${mintAddress}`
+  )} | ${link(
+    'SOLSCAN', `https://solscan.io/token/${mintAddress}`
+  )}`
+}
+
+/**
+ * 输出 Swap 消息
+ * @param name
+ * @param symbol
+ * @param mintAddress
+ * @param tx
+ */
+export function renderSwap(name: string, symbol: string, mintAddress: string, tx: string) {
+  return format`
+   Swap 0.0005SOL for ${bold(name)}($${bold(symbol)})
+   ${bold(mintAddress)}
+   Links: ${link(
+    'GMGN.ai', `https://gmgn.ai/sol/token/${mintAddress}`
+  )} | ${link(
+    'TX', `https://solscan.io/tx/${tx}`
+  )}`
+}
