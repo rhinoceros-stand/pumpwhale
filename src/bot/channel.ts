1ea2053cf9633c8c7ab25c211857fdebb59d54ae
roofers_00_dribble <roofers_00_dribble@icloud.com>
2025-01-01 00:01:33 +0800

    feat: add channel script

--- src/bot/channel.ts
+++ src/bot/channel.ts
@@ -1,48 +1,75 @@
-import * as dotenv from 'dotenv'
 import { Connection } from '@solana/web3.js'
-import { bold, format, link } from '@gramio/format'
-import PumpFun from '../services/pumpfun'
-import Telegram from '../services/telegram'
+import * as dotenv from 'dotenv'
+import { Bot } from 'gramio'
+import Bonding from '../services/onchain/bonding'
+import { logger } from '../utils/logger'
+import { renderBonding } from './_utils/template'
 
 dotenv.config({
   path: ['.env.local', 'env']
 })
 
 const CHANNEL_NAME = '@pumpfun_bonding_alert'
-let connection: Connection
 
-try {
-  connection = new Connection(process.env.SOLANA_RPC_URL, 'confirmed')
-  console.info('Solana RPC Connection Success, Monitoring Pump.fun Bonding Address!')
-} catch (err) {
-  console.error('Solana RPC Connection Error', err)
+export default class BotChannel {
+  private readonly SOL_RPC_URL = process.env.SOLANA_RPC_URL
+  private readonly BOT_SECRET = process.env.TELEGRAM_BOT_SECRET
+
+  /**
+   * Telegram Bot
+   * @private
+   */
+  private _bot: Bot
+
+  private _conn: Connection
+
+  /**
+   * Bonding 服务
+   * 接收流动性注入通知
+   * @private
+   */
+  private _bonding: Bonding
+
+  constructor() {
+    this._conn = new Connection(this.SOL_RPC_URL, 'confirmed')
+    this._bot = new Bot(this.BOT_SECRET)
+  }
+
+  /**
+   * 初始化服务
+   */
+  async initService() {
+    this._bonding = new Bonding()
+    this._bonding.init(this._conn)
+    this._bonding.setCallback(this.onPairBonding.bind(this))
+  }
+
+  /**
+   *
+   */
+  start() {
+    this._bot.onStart((params) => {
+      logger.info(`Channel Bot ${params.info.first_name} running...`)
+    })
+
+    this._bonding.start()
+  }
+
+  /**
+   *
+   * @param data
+   */
+  async onPairBonding(data: any) {
+    const {
+      address,
+      name,
+      symbol
+    } = data
+
+    this._bot.api.sendMessage({
+      chat_id: CHANNEL_NAME,
+      text: renderBonding(name, symbol, address)
+    })
+  }
 }
 
-const telegram = new Telegram({
-  secret: process.env.TELEGRAM_BOT_SECRET
-})
-
-const onPairBonding = (tokenData) => {
-  telegram.bot.api.sendMessage({
-    chat_id: CHANNEL_NAME,
-    text:
-      format`
-        ${bold(tokenData.name)}($${bold(tokenData.symbol)})
-        ${bold(tokenData.address)}
-        Links: ${link(
-        'Pump.fun',
-        `https://pump.fun/coin/${tokenData.address}`
-      )} | ${link(
-        'GMGN.ai',
-        `https://gmgn.ai/sol/token/${tokenData.address}`
-      )} | ${link(
-        'SOLSCAN',
-        `https://solscan.io/token/${tokenData.address}`
-      )}`
-  })
-}
-
-new PumpFun({
-  connection,
-  onPairBonding
-}).start()
---
+++ src/scripts/channel.ts
@@ -0,0 +1,6 @@
+import BotChannel from '../bot/channel'
+
+const bot = new BotChannel()
+
+bot.initService()
+bot.start()
