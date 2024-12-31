b32815a7276ad1372fc55348fab128e047295b89
roofers_00_dribble <roofers_00_dribble@icloud.com>
2024-12-31 21:22:18 +0800

    feat: add token metadata

---
+++ src/services/onchain/metadata.ts
@@ -0,0 +1,27 @@
+import { Connection, PublicKey } from '@solana/web3.js'
+import { Metaplex } from '@metaplex-foundation/js'
+import { getMint } from '@solana/spl-token'
+
+export async function getTokenMeatData(mintAddress: PublicKey, conn: Connection) {
+  const metaplex = Metaplex.make(conn)
+  const mintInfo = await getMint(conn, mintAddress)
+
+  const metadataAccount = metaplex
+    .nfts()
+    .pdas()
+    .metadata({ mint: mintAddress })
+
+  const metadataAccountInfo = await conn.getAccountInfo(metadataAccount)
+  if (metadataAccountInfo) {
+    const token = await metaplex.nfts().findByMint({ mintAddress })
+
+    return {
+      name: token.name,
+      symbol: token.symbol,
+      supply: mintInfo.supply,
+      decimals: token.mint.decimals
+    }
+  } else {
+    return {}
+  }
+}
