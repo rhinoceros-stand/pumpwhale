5e6e5145218e846aa07bda7d2613b2f8566f8af6
roofers_00_dribble <roofers_00_dribble@icloud.com>
2024-12-31 23:05:49 +0800

    fix: using restapi to get price

--- src/services/quoted.ts
+++ src/services/quoted.ts
@@ -1,47 +1,28 @@
-import { WebsocketAPI } from '@binance/connector-typescript'
-import { get } from 'lodash'
+import { Spot } from '@binance/connector-typescript'
 import { logger } from '../utils/logger'
 
 export default class Quoted {
-  private readonly PRICE_TICKER_PAIR = 'SOLUSDT'
-  private _wsClient: WebsocketAPI
+  private _client
   private _timeId: Timer
   private SOL_PRICE: number = -1
   private _callback: (value: number) => void
 
   constructor({ onQuoteChange }) {
     this._callback = onQuoteChange
-    this._wsClient = new WebsocketAPI('', '', {
-      callbacks: {
-        open: () => {
-          logger.info('Connected to Binance WebSocket server')
-          this.handleGetQuote()
-        },
-        close: () => {
-          logger.info('Disconnected from WebSocket server')
-        },
-        message: this.onPriceMessage.bind(this)
-      }
-    })
+
+    this._client = new Spot('', '', { baseURL: 'https://api.binance.com' })
+    this.onFetchPrice()
   }
 
-  onPriceMessage(data: string) {
-    const parseData = JSON.parse(data)
-    const priceText = get(parseData, 'result.price')
-    this.SOL_PRICE = parseInt(priceText, 10)
-    logger.info(`SOLUSDT: ${priceText}`)
+  async onFetchPrice() {
+    const response = await this._client.currentAveragePrice('SOLUSDT')
+    this.SOL_PRICE = Number(response.price)
+    logger.info(`SOLUSDT: ${response.price}`)
 
     if (this._callback) {
       this._callback(this.SOL_PRICE)
     }
 
-    this._timeId = setTimeout(this.handleGetQuote.bind(this), 50000)
-  }
-
-  /**
-   * 获取报价
-   */
-  handleGetQuote() {
-    this._wsClient.avgPrice(this.PRICE_TICKER_PAIR)
+    this._timeId = setTimeout(this.onFetchPrice.bind(this), 1000 * 60 * 10)
   }
 }
