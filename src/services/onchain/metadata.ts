450d5f2712e14901bacd16b62917d9e1f3ed0151
roofers_00_dribble <roofers_00_dribble@icloud.com>
2024-12-31 22:22:49 +0800

    fix: submit tx

--- src/services/onchain/metadata.ts
+++ src/services/onchain/metadata.ts
@@ -1,6 +1,6 @@
 import { Connection, PublicKey } from '@solana/web3.js'
+import { getMint, TOKEN_PROGRAM_ID } from '@solana/spl-token'
 import { Metaplex } from '@metaplex-foundation/js'
-import { getMint } from '@solana/spl-token'
 
 export async function getTokenMeatData(mintAddress: PublicKey, conn: Connection) {
   const metaplex = Metaplex.make(conn)
@@ -25,4 +25,32 @@ export async function getTokenMeatData(mintAddress: PublicKey, conn: Connection)
   } else {
     return {}
   }
+}
+
+/**
+ * 获取代币持有人数
+ * @param mintAddress
+ * @param conn
+ */
+export async function getTokenHolders(mintAddress: PublicKey, conn: Connection) {
+  const accounts = await conn.getParsedProgramAccounts(TOKEN_PROGRAM_ID, {
+    commitment: 'confirmed',
+    filters: [
+      {
+        dataSize: 165 // Number of bytes for a token account
+      },
+      {
+        memcmp: {
+          offset: 0, // Offset to start reading the token mint address
+          bytes: mintAddress.toBase58() // The token mint address
+        }
+      }
+    ]
+  })
+
+  const holders = accounts
+    .filter((account) => account.account.data.parsed.info.tokenAmount.uiAmount > 0)
+    .map((account) => account.pubkey.toBase58())
+
+  return holders.length
 }
--- src/services/onchain/trading.ts
+++ src/services/onchain/trading.ts
@@ -2,9 +2,9 @@ import { Wallet } from '@project-serum/anchor'
 import { Connection, Keypair, LAMPORTS_PER_SOL, Logs, PublicKey } from '@solana/web3.js'
 import bs58 from 'bs58'
 import queryString from 'query-string'
-import { OnChainService } from './index'
+import { getTokenHolders, getTokenMeatData } from './metadata'
 import { logger } from '../../utils/logger'
-import { getTokenMeatData } from './metadata'
+import { OnChainService } from './index'
 
 const SOL_MINT_ADDRESS = 'So11111111111111111111111111111111111111112'
 
@@ -63,7 +63,9 @@ export default class Trading implements OnChainService {
     // 查询代币报价
     const pairQuoteResponse = await this.getTokenPairQuote(address)
 
+    //
     const valueInUSD = this._solPrice * (SOL_ORDER_AMOUNT / LAMPORTS_PER_SOL)
+
     // 获取单价和市值
     const {
       price,
@@ -71,9 +73,107 @@ export default class Trading implements OnChainService {
       outAmount
     } = this.getPairPrice(pairQuoteResponse.outAmount, valueInUSD, tokenMetadata.supply, tokenMetadata.decimals)
 
-    logger.info(`Jupiter Quote: $${tokenMetadata.symbol}, ${valueInUSD} USD for ${outAmount}, Avg Price: ${price}, Market CAP: ${marketCapitalization}`)
+    logger.info(`Jupiter Quote: $${tokenMetadata.symbol}, ${valueInUSD} USD for ${outAmount}, Avg Price: ${price}, MCAP: ${this.displayMarketCapitalization(marketCapitalization)}`)
 
-    return ''
+    // 查询持有人数量
+    const holders = await getTokenHolders(mintAddress, this._conn)
+    logger.info(`$${tokenMetadata.symbol}(${tokenMetadata.address}) Holders: ${holders}`)
+
+    // 查询 TOP 20 持有量
+    const topHolders = await this._conn.getTokenLargestAccounts(mintAddress)
+
+    const holderMinimum = 200
+    if (holders < holderMinimum) {
+      logger.warning(`$${tokenMetadata.symbol}(${tokenMetadata.address}) Holders is less than ${holderMinimum}, ignore for swap.`)
+      return
+    }
+
+    const tokenValueMinimum = 85000
+    if (marketCapitalization < tokenValueMinimum) {
+      logger.warning(`$${tokenMetadata.symbol}(${tokenMetadata.address}) market capitalization is less than ${tokenValueMinimum}, ignore for swap.`)
+      return
+    }
+
+    // 准备交易
+    const tx = await this.preparingSwap(pairQuoteResponse)
+
+    // 根据 transaction 查询交换的代币数量，输出单价
+    const transactionInfo = await this._conn.getParsedTransaction(tx, {
+      maxSupportedTransactionVersion: 0
+    })
+
+    const { meta } = transactionInfo
+    const filterBalance = meta.postTokenBalances.findLast(v => v.owner === this._wallet.publicKey.toBase58())
+    if (filterBalance) {
+      const executeSwap = {
+        amount: filterBalance.uiTokenAmount,
+        price: valueInUSD / filterBalance.uiTokenAmount.uiAmount
+      }
+
+      logger.info(`$${tokenMetadata.symbol}(${tokenMetadata.address}) swap success, ${valueInUSD} USD for ${executeSwap.amount}, Avg Price: ${executeSwap.price}`)
+    }
+
+    return tx
+  }
+
+  /**
+   * 准备交换
+   * @param quoteResponse 报价信息
+   */
+  async preparingSwap(quoteResponse: any) {
+    // get serialized transactions for the swap
+    const { swapTransaction } = await (
+      await fetch('https://quote-api.jup.ag/v6/swap', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          // quoteResponse from /quote api
+          quoteResponse,
+          // user public key to be used for the swap
+          userPublicKey: this._wallet.publicKey.toString(),
+          // auto wrap and unwrap SOL. default is true
+          wrapAndUnwrapSol: true,
+          // jup.ag frontend default max for user
+          dynamicSlippage: {
+            'maxBps': SLIPPAGE_DEFAULT_BPS
+          }
+          // feeAccount is optional. Use if you want to charge a fee.  feeBps must have been passed in /quote API.
+          // feeAccount: "fee_account_public_key"
+        })
+      })
+    ).json()
+
+    // deserialize the transaction
+    const swapTransactionBuf = Buffer.from(swapTransaction, 'base64')
+    // @ts-ignore
+    const transaction = VersionedTransaction.deserialize(swapTransactionBuf)
+
+    // sign the transaction
+    transaction.sign([this._wallet.payer])
+
+    // get the latest block hash
+    const latestBlockHash = await this._conn.getLatestBlockhash()
+
+    try {
+      // Execute the transaction
+      const rawTransaction = transaction.serialize()
+      const txid = await this._conn.sendRawTransaction(rawTransaction, {
+        skipPreflight: false
+      })
+
+      // 查询交易是否成功
+      const confirmInfo = await this._conn.confirmTransaction({
+        blockhash: latestBlockHash.blockhash,
+        lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+        signature: txid
+      })
+
+      return txid
+    } catch (err) {
+      logger.error(`Send swap request error`, err)
+    }
   }
 
   /**
@@ -98,17 +198,6 @@ export default class Trading implements OnChainService {
   }
 
   /**
-   * 计算下单均价
-   * @param outAmount
-   */
-  getAvgPrice(outAmount: number) {
-    const valueInSOL = this._solPrice * (SOL_ORDER_AMOUNT / LAMPORTS_PER_SOL)
-    console.log(valueInSOL, outAmount)
-
-    return valueInSOL / outAmount
-  }
-
-  /**
    * 计算交易对价格
    * @param outAmount
    * @param inputValue
@@ -125,6 +214,28 @@ export default class Trading implements OnChainService {
       outAmount: amount,
       price: inputValue / (Number(outAmount) / decimalWithZero),
       marketCapitalization: price * (Number(supply) / decimalWithZero)
+    }
+  }
+
+  /**
+   * 市值显示转换
+   * @param value
+   */
+  displayMarketCapitalization(value: number) {
+    let getValue = value
+    if (value > 1000000) {
+      getValue = value / 1000000
+
+      return `${Math.trunc(getValue)}M`
+    }
+    if (value < 1000000) {
+      getValue = value / 1000
+
+      return `${Math.trunc(getValue)}K`
+    }
+
+    if (value < 100000) {
+      return `${Math.trunc(getValue)}K`
     }
   }
 }
