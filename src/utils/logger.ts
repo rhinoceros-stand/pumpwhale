f07e29004b127518304250437d06dcb4edbf1ac0
roofers_00_dribble <roofers_00_dribble@icloud.com>
2024-12-31 17:36:22 +0800

    feat: add logger

---
+++ src/utils/logger.ts
@@ -0,0 +1,36 @@
+import {
+  createLogger,
+  format,
+  transports
+} from 'winston'
+
+const { combine, timestamp, label, printf } = format
+
+const myFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp} ${level}: ${message}`
+})
+
+const logger = createLogger({
+  level: 'info',
+  format: combine(
+    timestamp(),
+    myFormat
+  ),
+  transports: [
+    new transports.Console(),
+    //
+    // - Write all logs with importance level of `error` or higher to `error.log`
+    //   (i.e., error, fatal, but not other levels)
+    //
+    new transports.File({ filename: 'error.log', level: 'error' }),
+    //
+    // - Write all logs with importance level of `info` or higher to `combined.log`
+    //   (i.e., fatal, error, warn, and info, but not trace)
+    //
+    new transports.File({ filename: 'combined.log' })
+  ]
+})
+
+export {
+  logger
+}
